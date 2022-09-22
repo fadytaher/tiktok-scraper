9b8752d595c63fe3a5571568afc1321004390d10
fadytaher <fady.taher@izea.com>
2022-09-22 04:11:31 +0200

    revert old headers

--- src/core/TikTok.ts
+++ src/core/TikTok.ts
@@ -1218,7 +1218,15 @@ export class TikTokScraper extends EventEmitter {
             'connection': 'keep-alive',
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "accept-language": "en-US,en;q=0.9,ar;q=0.8,de;q=0.7",
-            "Accept-Encoding":"gzip, deflate, br",
+            "cache-control": "max-age=0",
+            "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Google Chrome\";v=\"101\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1"
         }
         };
 
